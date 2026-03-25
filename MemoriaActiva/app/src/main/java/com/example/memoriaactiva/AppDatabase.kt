9028d5591c4ca6b814a8e042eed38020853b58b1
Eduardo Ledesma <eduardoledesmav@gmail.com>
2026-03-25 15:58:08 -0700

    DB Citas

--- MemoriaActiva/app/src/main/java/com/example/memoriaactiva/AppDatabase.kt
+++ MemoriaActiva/app/src/main/java/com/example/memoriaactiva/AppDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Medicamento::class], version = 1)
+@Database(entities = [Medicamento::class, Cita::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun medicamentoDao(): MedicamentoDao
+    abstract fun citaDao(): CitaDao
 
     companion object {
         @Volatile
@@ -19,7 +20,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "memoria_activa_db"
-                ).build()
+                )
+                .fallbackToDestructiveMigration() // Útil durante el desarrollo para evitar crashes al cambiar la DB
+                .build()
                 INSTANCE = instance
                 instance
             }
